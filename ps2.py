da24426e67ebcdb6180c37a94d8b049444e71210
Noah Haskell <haskell16n@ncssm.edu>
2015-10-19 22:28:38 -0400

    Base ps2.py w/ name and date

---
+++ ps2.py
@@ -0,0 +1,124 @@
+##########################################
+#   
+#   Author:  Noah Haskell
+#   Date Created: 10-19-2015
+#   Date Last Modified
+#   Program Name: ps2.py
+#
+##########################################
+import os            ##You will want these for access
+import os.path       ##to the file system. See the documentation 
+
+#1
+def fibToMe(l):
+    """precondition: l is an integer
+postcondition: return the number in the fibonacci series which is the first 
+with l or more digits.
+fibonacci number: 0 1 1 2 3 5 8 13 21 34 55 89 144
+number in series: 0 1 2 3 4 5 6 7  8  9  10 11 12
+fibToMe(2) -> 7
+fibToMe(3) -> 12
+"""
+    return 0
+
+
+#2
+def specialSum(n):
+    """precondition: n is a positive integer.
+postconditions: return the sum of all integers 1 to n (inclusive) divisible by 
+2 or 3, but not 5.
+specialSum(10) -> 32
+specialSum(20) -> 92
+"""
+    return 0
+
+
+#3
+def fileContains(filename, word, recurse=False):
+    """precondition:  filename and word are strings.
+postcondition:  If the file filename is a directory, inspect the contents of 
+all files in the directory and return True if word appears in any of them.
+If the filename is a regular file and if the string word is found in the 
+file return True
+If the file filename does not exist, return False
+Otherwise, return False
+##Challenge: implement the recurse option to recursively check files in 
+subdirectories if recurse is True
+    """
+    return False
+
+
+#4
+def countFilesWithExtension(directory, extension):
+    """prec: directory is a string, extension is a string.
+postc:  return a -1 if the directory does not exist.
+Otherwise return a count of all files in the directory with extension
+.extension (be nice to the user...  prepend a '.' if they omit it.  if they 
+include it, do not double it)
+"""
+    return 0
+
+
+#5
+def assort(counts):
+    """prec:  counts is a dictionary whose keys are words
+    and whose values are positive integers.
+    postc:  return a dictionary whose keys are integers
+    and whose values are lists containing all words paired
+    with the integers, sorted alphbeticallly.  Here is an example
+    d = {"cat":3, "dog":1, "pig":1, "horse":3, "lemur":4}
+    assort(d) -> {1: ["dog", "pig"], 3:["cat", "dog"], 4:["lemur"]}
+    """
+    return {}
+
+
+#6
+def aMatchMadeAtNCSSM(username):
+    """precondition: username is a valid username on the cs server (you can use
+your username to test)
+postcondition: return a list containing 1 or more valid usernames that share
+the most characters (apositionally) with the given username (duplicate letters
+should only boost the count if they are duplicated in both names)
+You may use home directory names as a valid proxy for usernames.  Remember that
+user home directories live in: /home/ and associated subdirectories
+""" 
+    return []
+
+
+#7
+def ionics(filename, gimmieGimmie, delim=","):
+    """precondition: filename is a valid filename.  filename's contents are 
+columnar data delimited by a character, delim (by default ',').  The columns 
+are numbered from 0 to n-1 (with n being the number of columns).
+gimmieGimmie is a list of integers that are valid column numbers in the file.
+postcondition: create a file with the same name as the original with a ".out" 
+extension.  This file should contain the columns requested via gimmieGimmie,
+delimited by delim.
+sampleFile.csv:
+0,0,,1,1,0,
+1,2,3,4,5,6,7
+7,6,5,4,3,2,1
+cat,dog,bird,fish,rat,horse,cow
+this,that,other,another,else,further,farther
+
+ionics(sampleFile.csv, [2,4,6])
+will create this file
+sampleFile.csv.out
+,4,
+3,5,7
+5,3,1
+bird,rat,cow
+other,else,farther
+"""
+    pass
+
+
+#7 Bonus
+def autoDetectDelimitor(filename):
+    """precondition: filename is a columnar file as above.
+postcondition: autodetect and return the single character delimitor to the best of your ability.
+if you are unable to detect, return None
+"""
+    return None
+
+
